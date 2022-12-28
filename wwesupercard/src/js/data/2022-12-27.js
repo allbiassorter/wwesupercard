8bbaf0c8379245dd47d31241a536fe5e504d2452
allbiassorter <120835373+allbiassorter@users.noreply.github.com>
2022-12-28 12:38:30 -0500

    Add files via upload

---
+++ wwesupercard/src/js/data/2022-12-27.js
@@ -0,0 +1,488 @@
+dataSetVersion = "2022-12-27"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Seasons",
+    key: "season",
+    tooltip: "Check this to restrict to certain season.",
+    checked: false,
+    sub: [
+      {
+        name: "Season 1",
+        tooltip: "S1",
+        key: "S1",
+      },
+      {
+        name: "Season 2",
+        tooltip: "S2",
+        key: "S2",
+      },
+      {
+        name: "Season 3",
+        tooltip: "S3",
+        key: "S3",
+      },
+      {
+        name: "Season 4",
+        tooltip: "S4",
+        key: "S4",
+      },
+      {
+        name: "Season 5",
+        tooltip: "S5",
+        key: "S5",
+      },
+      {
+        name: "Season 6",
+        tooltip: "S6",
+        key: "S6",
+      },
+      {
+        name: "Season 7",
+        tooltip: "S7",
+        key: "WWII",
+      },
+      {
+        name: "Season 8",
+        tooltip: "S8",
+        key: "S8",
+      },
+      {
+        name: "Season 9",
+        tooltip: "S9",
+        key: "S9",
+      },
+    ],
+  },
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Common (S1)",
+    img: "LrssjDD.png",
+    opts: {
+      season: ["S1"],
+    },
+  },
+  {
+    name: "Uncommon (S1)",
+    img: "vxOkvnb.png",
+    opts: {
+      season: ["S1"],
+    },
+  },
+  {
+    name: "Rare (S1)",
+    img: "RszfvUM.png",
+    opts: {
+      season: ["S1"],
+    },
+  },
+  {
+    name: "Super Rare (S1)",
+    img: "mYvqZ1I.png",
+    opts: {
+      season: ["S1"],
+    },
+  },
+  {
+    name: "Ultra Rare (S1)",
+    img: "hGIhG0S.png",
+    opts: {
+      season: ["S1"],
+    },
+  },
+  {
+    name: "Epic (S1)",
+    img: "ir62JvV.png",
+    opts: {
+      season: ["S1"],
+    },
+  },
+  {
+    name: "Legendary (S1)",
+    img: "8zrBgEs.png",
+    opts: {
+      season: ["S1"],
+    },
+  },
+  {
+    name: "Survivor (S1)",
+    img: "AhtTiWM.png",
+    opts: {
+      season: ["S1"],
+    },
+  },
+  {
+    name: "WrestleMania (S1)",
+    img: "o3yENZE.png",
+    opts: {
+      season: ["S1"],
+    },
+  },
+  {
+    name: "Common (S2)",
+    img: "YC9V51D.png",
+    opts: {
+      season: ["S2"],
+    },
+  },
+  {
+    name: "Uncommon (S2)",
+    img: "CVUDY0D.png",
+    opts: {
+      season: ["S2"],
+    },
+  },
+  {
+    name: "Rare (S2)",
+    img: "RC3545R.png",
+    opts: {
+      season: ["S2"],
+    },
+  },
+  {
+    name: "Super Rare (S2)",
+    img: "P5jsAP3.png",
+    opts: {
+      season: ["S2"],
+    },
+  },
+  {
+    name: "Ultra Rare (S2)",
+    img: "Tlu71KU.png",
+    opts: {
+      season: ["S2"],
+    },
+  },
+  {
+    name: "Epic (S2)",
+    img: "sBQTYx4.png",
+    opts: {
+      season: ["S2"],
+    },
+  },
+  {
+    name: "Legendary (S2)",
+    img: "4c8mEfB.png",
+    opts: {
+      season: ["S2"],
+    },
+  },
+  {
+    name: "Survivor (S2)",
+    img: "6GTlUYF.png",
+    opts: {
+      season: ["S2"],
+    },
+  },
+  {
+    name: "WrestleMania (S2)",
+    img: "Qd3eCJC.png",
+    opts: {
+      season: ["S2"],
+    },
+  },
+  {
+    name: "SummerSlam",
+    img: "LaPouVu.png",
+    opts: {
+      season: ["S2"],
+    },
+  },
+  {
+    name: "Hardened",
+    img: "KIIEahc.png",
+    opts: {
+      season: ["S3"],
+    },
+  },
+  {
+    name: "Elite",
+    img: "PsoE7Or.png",
+    opts: {
+      season: ["S3"],
+    },
+  },
+  {
+    name: "Ultimate",
+    img: "igfmtyy.png",
+    opts: {
+      season: ["S3"],
+    },
+  },
+  {
+    name: "WrestleMania 33",
+    img: "I04r30v.png",
+    opts: {
+      season: ["S3"],
+    },
+  },
+  {
+    name: "SummerSlam '17",
+    img: "6qfLKdm.png",
+    opts: {
+      season: ["S3"],
+    },
+  },
+  {
+    name: "Beast",
+    img: "cLO2Qpj.png",
+    opts: {
+      season: ["S4"],
+    },
+  },
+  {
+    name: "Monster",
+    img: "oq5JhYQ.png",
+    opts: {
+      season: ["S4"],
+    },
+  },
+  {
+    name: "Titan",
+    img: "xHMswki.png",
+    opts: {
+      season: ["S4"],
+    },
+  },
+  {
+    name: "WrestleMania 34",
+    img: "DO6AT2H.png",
+    opts: {
+      season: ["S4"],
+    },
+  },
+  {
+    name: "Goliath",
+    img: "86y7Dd4.png",
+    opts: {
+      season: ["S4"],
+    },
+  },
+  {
+    name: "SummerSlam '18",
+    img: "hU3yQ2c.png",
+    opts: {
+      season: ["S4"],
+    },
+  },
+  {
+    name: "Gothic",
+    img: "zspbQ18.png",
+    opts: {
+      season: ["S5"],
+    },
+  },
+  {
+    name: "Neon",
+    img: "M9k77p6.png",
+    opts: {
+      season: ["S5"],
+    },
+  },
+  {
+    name: "WrestleMania 35",
+    img: "AfqpW1s.png",
+    opts: {
+      season: ["S5"],
+    },
+  },
+  {
+    name: "Cataclysm",
+    img: "pJWRRZ9.png",
+    opts: {
+      season: ["S5"],
+    },
+  },
+  {
+    name: "SummerSlam '19",
+    img: "gKdQjnD.png",
+    opts: {
+      season: ["S5"],
+    },
+  },
+  {
+    name: "Nightmare",
+    img: "nh6TdId.png",
+    opts: {
+      season: ["S6"],
+    },
+  },
+  {
+    name: "Primal",
+    img: "DM3vZqJ.png",
+    opts: {
+      season: ["S6"],
+    },
+  },
+  {
+    name: "Vanguard",
+    img: "EjizJMn.png",
+    opts: {
+      season: ["S6"],
+    },
+  },
+  {
+    name: "Royal Rumble",
+    img: "M3KkRBz.png",
+    opts: {
+      season: ["S6"],
+    },
+  },
+  {
+    name: "WrestleMania 36",
+    img: "gBkP0o3.png",
+    opts: {
+      season: ["S6"],
+    },
+  },
+  {
+    name: "Elemental",
+    img: "3V6TQ8v.png",
+    opts: {
+      season: ["S6"],
+    },
+  },
+  {
+    name: "SummerSlam '20",
+    img: "pfoyWce.png",
+    opts: {
+      season: ["S6"],
+    },
+  },
+  {
+    name: "Bio-Mechanical",
+    img: "p6Jc5dq.png",
+    opts: {
+      season: ["S7"],
+    },
+  },
+  {
+    name: "Swarm",
+    img: "pby6tuI.png",
+    opts: {
+      season: ["S7"],
+    },
+  },
+  {
+    name: "Behemoth",
+    img: "M2YYNUO.png",
+    opts: {
+      season: ["S7"],
+    },
+  },
+  {
+    name: "Royal Rumble '21",
+    img: "XMir5e6.png",
+    opts: {
+      season: ["S7"],
+    },
+  },
+  {
+    name: "WrestleMania 37",
+    img: "v6uce5X.png",
+    opts: {
+      season: ["S7"],
+    },
+  },
+  {
+    name: "Forged",
+    img: "rKHy52k.png",
+    opts: {
+      season: ["S7"],
+    },
+  },
+  {
+    name: "SummerSlam '21",
+    img: "V3u4deU.png",
+    opts: {
+      season: ["S7"],
+    },
+  },
+  {
+    name: "Mire",
+    img: "pW7lw4J.png",
+    opts: {
+      season: ["S8"],
+    },
+  },
+  {
+    name: "Maelstorm",
+    img: "kNDqTOA.png",
+    opts: {
+      season: ["S8"],
+    },
+  },
+  {
+    name: "Valhalla",
+    img: "oZzeFRh.png",
+    opts: {
+      season: ["S8"],
+    },
+  },
+  {
+    name: "Royal Rumble '22",
+    img: "bpN1Jxe.png",
+    opts: {
+      season: ["S8"],
+    },
+  },
+  {
+    name: "Road to WrestleMania",
+    img: "ERvOAg2.png",
+    opts: {
+      season: ["S8"],
+    },
+  },
+  {
+    name: "WrestleMania 38",
+    img: "8eFqoU8.png",
+    opts: {
+      season: ["S8"],
+    },
+  },
+  {
+    name: "Ronin",
+    img: "jzHbS0R.png",
+    opts: {
+      season: ["S8"],
+    },
+  },
+  {
+    name: "SummerSlam BCE",
+    img: "smzzPHm.png",
+    opts: {
+      season: ["S8"],
+    },
+  },
+  {
+    name: "Arcane",
+    img: "JZk4Ur7.png",
+    opts: {
+      season: ["S8"],
+    },
+  },
+  {
+    name: "Pixel",
+    img: "FdH56Mg.png",
+    opts: {
+      season: ["S9"],
+    },
+  },
+  {
+    name: "Extinction",
+    img: "NXkqde6.png",
+    opts: {
+      season: ["S9"],
+    },
+  },
+  {
+    name: "Octane",
+    img: "sIEU4rx.png",
+    opts: {
+      season: ["S9"],
+    },
+  },
+];
